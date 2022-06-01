94e5d326d1e93b0326eb84b622c56e0c1a9f4b12
Kevin <mr.mcelhinney@gmail.com>
2022-05-31 20:33:25 -0700

    created login route and controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:username])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json:user, status: :created
+    else
+      render json: {error: {login: "Invalid Email or Password"}}, status: :unauthorized
+    end
+  end
+end
